2afc422dc3405d366e6df8df954b18064bcf5d46
zun5761 <zun5761@gmail.com>
2025-04-16 15:40:00 +0900

    페이지 이동 확인인

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ function Home() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <Button to="/about">go to about</Button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
