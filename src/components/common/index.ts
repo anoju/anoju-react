9af110540a4176b7ff5b4cab9085f6d21d3e948b
anoju <zun5761@gmail.com>
2025-05-14 09:56:57 +0900

    common 컴포넌트 인덱스에 Textarea 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,5 @@ export { default as CodeHighlight } from './CodeHighlight';
 export { default as Tabs, Tab, TabPanel } from './Tabs';
 export { default as Checkbox, CheckboxGroup } from './Checkbox';
 export { default as Radio, RadioGroup } from './Radio';
-export { default as Input } from './Input';
-export { default as Textarea } from './Textarea';
 export { default as Tooltip } from './Tooltip';
+export { default as Textarea } from './Textarea';
